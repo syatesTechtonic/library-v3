8a95111e483efe1bd1b7475f4833bf4dd57e63c2
syatesTechtonic <stuart.yates@techtonic.com>
2020-01-15 13:34:42 -0700

    refactoring sections

--- scripts/bk-table.js
+++ scripts/bk-table.js
@@ -1,7 +1,12 @@
-let $row = $('.js-bk-table__row');
+let $row = $('.js-bk-title__row');
 let info   = '.js-bk-info';
 
+$(document).ready(function(){
+  $(info).hide();
+});
+
 $row.click(function () {
+  $(this).next(info).children().first().toggleClass('bk-info--open');
   $(this).next(info).slideToggle();
   $(this).parent().siblings().children().next().slideUp();
   return false;
