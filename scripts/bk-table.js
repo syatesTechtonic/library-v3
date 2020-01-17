62b3db287c25027046f90c4cabd27b54e90ae6a4
syatesTechtonic <stuart.yates@techtonic.com>
2020-01-17 11:21:14 -0700

    table populate complete, working on search

--- scripts/bk-table.js
+++ scripts/bk-table.js
@@ -1,13 +1,85 @@
-let $row = $('.js-bk-header');
-let info   = '.js-bk-info';
+const books = [
+  {
+    id: "111aaa",
+    author: "Dr Seuss",
+    title: "The Cat in the Hat",
+    imgSrc: "assets/images/covers/cat-hat.png",
+    imgAlt: "cat-hat-cover",
+    pages: 61,
+    pubDate: "3/12/1957",
+    synopsis: "When in doubt, turn to the story of the cat that transformed a dull, rainy afternoon into a magical and just-messy-enough adventure.",
+    rating: 4
+  },
+  {
+    id: "222bbb",
+    author: "Dr Seuss",
+    title: "Green Eggs and Ham",
+    imgSrc: "assets/images/covers/green-eggs.png",
+    imgAlt: "green-eggs-cover",
+    pages: 65,
+    pubDate: "8/12/1960",
+    synopsis: "Sam-I-am is as persistent as a telemarketer, changing as many variables as possible in the hopes of convincing the nameless skeptic that green eggs and ham are a delicacy to be savored.",
+    rating: 3
+  },
+  {
+    id: "333ccc",
+    author: "Dr Seuss",
+    title: "There's a Wocket in my Pocket",
+    imgSrc: "assets/images/covers/wocket-pocket.png",
+    imgAlt: "wocket-pocket-cover",
+    pages: 65,
+    pubDate: "8/12/1984",
+    synopsis: "A young boy goes exploring in his house and finds an array of fun characters! Are you certain there’s a Jertain in the curtain? Or have you ever had a feeling there’s a Geeling on the ceiling?",
+    rating: 5
+  },
+]
 
-$(document).ready(function(){
-  $(info).hide();
-});
+function onInit () {
+  books.map(book => addBook(book));
+  return;
+}
 
-$row.click(function () {
-  $(this).next(info).children().first().addClass('bk-info__content--open');
-  $(this).next(info).slideToggle();
-  $(this).parent().siblings().children().next().slideUp();
-  return false;
-});
+function addBook (book) {
+  const headerId = '#bk-' + book.id + '-header';
+  const infoId = '#bk-' + book.id + '-info';
+  $('#bkTable').append(`<header id="bk-${book.id}-header" class="bk-info__header js-bk-header"></header>`);
+  $('#bkTable').append(`<article id="bk-${book.id}-info" class="bk-info js-bk-info"></article>`);
+  $(headerId).append(`<h3 class="bk-info__author">${book.author}</h3><h3 class="bk-info__title">${book.title}</h3>`).click(() => clickTitle(headerId, infoId));
+  $(infoId).append(`<div class="bk-info__cover-rating"></div><div class="bk-info__copy">`);
+  $(infoId + ' div.bk-info__cover-rating').append(`<img src="${book.imgSrc}" alt="${book.imgAlt}" class="bk-info__cover"><div class="bk-info__rating">${stars(book.rating).map(star => `<span class="fa${star} fa-star"></span>`).join('')}</div>`);
+  $(infoId + ' div.bk-info__copy').append(`<p><span class="bk-info__copy--bold">Publication Date: </span><time datetime="${new Date(book.pubDate)}">${book.pubDate}</time></p><p><span class="bk-info__copy--bold">Pages: </span>${book.pages}</p><p><span class="bk-info__copy--bold">Synopsis: </span>${book.synopsis}</p>`);
+}
+
+function stars (rating) {
+  const numArray = [1,2,3,4,5];
+  return numArray.map(num => num <= rating ? 's' : 'r');
+}
+
+$(document).ready(() => onInit());
+
+function clickTitle (headerId, infoId) {
+  if ($(headerId).hasClass('bk-info__header--selected')) {
+    $(infoId).addClass('bk-info--open').slideUp();
+    return;
+  }
+  $(headerId).addClass('bk-info__header--selected');
+  $('#bkTable').children('.js-bk-header').not(headerId).removeClass('bk-info__header--selected');
+  $(infoId).hide().addClass('bk-info--open').slideDown();
+  $('#bkTable').children('.js-bk-info').not(infoId).removeClass('bk-info--open').slideUp();
+  return;
+}
+
+function checkBook (input, book) {
+  return input.title && book.title.toLowerCase().includes(input.title.toLowerCase()) || input.author && book.author.toLowerCase().includes(input.author.toLowerCase()) ? true : false;
+}
+
+$('#bookSearch').submit(function (e) {
+  e.preventDefault();
+  const input = {
+    title: $('[name="title"]').val() || '',
+    author: $('[name="author"]').val() || ''
+  }
+  const results = books.filter(book => checkBook(input, book))
+  console.log(results);
+  return results;
+})
